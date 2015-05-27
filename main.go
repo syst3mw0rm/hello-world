176f99320904bc2ce9faa9e3b8492f86cc39c249
Aamir Khan <syst3m.w0rm@gmail.com>
2015-05-28 01:21:24 +0900

    add debug output

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func all_tx(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer db.Close()
-	Fmt.Fprintln("querying all data...")
+	fmt.Fprintln("querying all data...")
 	rows, err := db.Query("SELECT * FROM tx;")
 	if err != nil {
 		panic(err)
